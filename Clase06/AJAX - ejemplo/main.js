8ba2e57fed2044f335db7ce229674c30b6604421
koalter <heavens_drive090@hotmail.com>
2019-05-07 22:14:18 -0300

    minimo ejemplo get de ajax

---
+++ Clase06/AJAX - ejemplo/main.js
@@ -0,0 +1,22 @@
+/*  Para iniciar necesitariamos utilizar NodeJS y descargar los modulos.
+*   En la terminal ingresariamos 'node server'
+*/
+
+var xhr;
+window.addEventListener('load', traerObjeto);
+
+function traerObjeto() {
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = procesar;
+    let cadena = 'http://localhost:3000/traer?collection=objeto';
+    xhr.open('GET', cadena, true);
+    xhr.send();
+}
+
+function procesar() {
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+            console.log(JSON.parse(xhr.responseText));
+        }
+    }
+}
